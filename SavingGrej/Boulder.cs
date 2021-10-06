d3d4f6a967ae4e7ef5bffab19ee482b2698daa5a
Oliver <oliver.nordin@elev.ga.ntig.se>
2021-10-06 14:58:07 +0200

    first comitt

---
+++ SavingGrej/Boulder.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Boulder
+{
+    public string Name { get; set; }
+    public int Weight { get; set; }
+    public int Density { get; set; }
+}
---
+++ SavingGrej/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace SavingGrej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boulder stone = new Boulder();
+            GetValues(stone);
+            string json = JsonSerializer.Serialize<Boulder>(stone);
+            File.WriteAllText("json.json", json);
+        }
+
+        static void GetValues(Boulder b)
+        {
+            Console.WriteLine("What's your boulders name?");
+            b.Name = Console.ReadLine();
+            Console.WriteLine("How much does your boulder weigh?");
+            b.Weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What's the density of your boulder?");
+            b.Density = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
